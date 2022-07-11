584dff3640a0db3d913eb2f1e5c7bd52839f6676
Akhlis <kreasify@gmail.com>
2022-07-11 23:39:36 +0700

    fix ui issue on archive

--- resources/views/dashboard/SwedishAmericanChurchArchiveRecord/records.blade.php
+++ resources/views/dashboard/SwedishAmericanChurchArchiveRecord/records.blade.php
@@ -2,145 +2,147 @@
     <!-- Main 3 column grid -->
     <div class="grid grid-cols-1 gap-4 items-start lg:gap-8">
         <!-- Left column -->
-        <section aria-labelledby="section-1-title">
+        <section class="pt-6" aria-labelledby="section-1-title">
+            <div class="bg-white py-6 pl-4 pr-3 border-gray-300 shadow md:rounded-lg">
+                <p class="text-left text-sm font-semibold text-white pb-4">
+                    Svenskamerikanska kyrkoarkivet
+                </p>
 
-            <div class="mt-8 flex flex-col">
-                <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
-                    <div class="bg-white py-4 pl-4 pr-3 border-gray-300 shadow-sm">
-                        <p class="text-left text-sm font-semibold text-white pb-4">
-                            Svenskamerikanska kyrkoarkivet
-                        </p>
+                @if(auth()->user()->hasRole('regular user|subscriber'))
+                @if(isset($keywords))
+                {!! Form::model($keywords,['route' => 'sacar.search']) !!}
+                @endif
+                @if(!isset($keywords))
+                {!! Form::open(['route' => 'sacar.search']) !!}
+                @endif
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="sm:grid sm:grid-cols-3 sm:items-start">
+                        <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                            {{ __('First name') }}</label>
+                        <div class="mt-1 sm:mt-0 sm:col-span-2">
 
-                        @if(auth()->user()->hasRole('regular user|subscriber'))
-                            @if(isset($keywords))
-                                {!! Form::model($keywords,['route' => 'sacar.search'])  !!}
-                            @endif
-                            @if(!isset($keywords))
-                                {!! Form::open(['route' => 'sacar.search'])  !!}
-                            @endif
-                            <div class="grid grid-cols-2 gap-4">
-                                <div class="sm:grid sm:grid-cols-3 sm:items-start">
-                                    <label for="first_name"
-                                           class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                        {{ __('First name') }}</label>
-                                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                            {!! Form::text('first_name', old('first_name'),
+                            ['class' => 'max-w-lg block w-full shadow-sm focus:ring-indigo-500
+                            focus:border-indigo-500
+                            sm:max-w-xs sm:text-sm border-gray-300 rounded-md',
+                            'id' => 'first_name']) !!}
+                            @error('first_name')
+                            <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}
+                            </p>@enderror
+                        </div>
+                    </div>
 
-                                        {!! Form::text('first_name', old('first_name'),
-                                                ['class' => 'max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500
-                                                sm:max-w-xs sm:text-sm border-gray-300 rounded-md',
-                                                'id' => 'first_name']) !!}
-                                        @error('first_name')
-                                        <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}
-                                        </p>@enderror
-                                    </div>
-                                </div>
+                    <div class="sm:grid sm:grid-cols-3 sm:items-start">
+                        <label for="last_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                            {{ __('Last name') }}</label>
+                        <div class="mt-1 sm:mt-0 sm:col-span-2">
 
-                                <div class="sm:grid sm:grid-cols-3 sm:items-start">
-                                    <label for="last_name"
-                                           class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                                        {{ __('Last name') }}</label>
-                                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                            {!! Form::text('last_name', null,
+                            ['class' => 'max-w-lg block w-full shadow-sm focus:ring-indigo-500
+                            focus:border-indigo-500
+                            sm:max-w-xs sm:text-sm border-gray-300 rounded-md',
+                            'id' => 'last_name']) !!}
+                            @error('last_name')
+                            <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}
+                            </p>@enderror
+                        </div>
+                    </div>
 
-                                        {!! Form::text('last_name', null,
-                                                ['class' => 'max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500
-                                                sm:max-w-xs sm:text-sm border-gray-300 rounded-md',
-                                                'id' => 'last_name']) !!}
-                                        @error('last_name')
-                                        <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}
-                                        </p>@enderror
-                                    </div>
-                                </div>
+                    @include('dashboard._filtersattributes')
 
-                                @include('dashboard._filtersattributes')
-
-                                <div class="sm:flex justify-around">
-                                    <span></span>
-                                    <button type="submit" name="action" value="search" class="inline-flex items-center px-8 py-2 border
+                    <div class="sm:flex justify-around">
+                        <span></span>
+                        <button type="submit" name="action" value="search" class="inline-flex items-center px-8 py-2 border
                                         border-transparent text-base font-medium rounded-md shadow-sm text-white
                                         bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2
                                         focus:ring-offset-2 focus:ring-indigo-500">{{ __('Search') }}</button>
-                                    <button type="submit" name="action" value="filter" class="inline-flex items-center px-8 py-2 border
+                        <button type="submit" name="action" value="filter" class="inline-flex items-center px-8 py-2 border
                                         border-transparent text-base font-medium rounded-md shadow-sm text-white
                                         bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2
                                         focus:ring-offset-2 focus:ring-indigo-500">{{ __('Filter') }}</button>
 
-                                </div>
-
-                            </div>
-                            {!! Form::close() !!}
-                        @endif
-
                     </div>
-                    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
 
-                                <table class="min-w-full table-auto border-separate" style="border-spacing: 0">
+                </div>
+                {!! Form::close() !!}
+                @endif
+
+            </div>
+            <div x-cloak class="mt-8">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+
+                            <table class="min-w-full table-auto border-separate" style="border-spacing: 0">
                                 <thead class="bg-gray-50">
-                                <tr>
-                                    <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
+                                    <tr>
+                                        <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
                                 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900
                                 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">{{ __('Full name') }}</th>
-                                    <th scope="col" class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50
+                                        <th scope="col" class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50
                                 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur
                                 backdrop-filter sm:table-cell"> {{ __('Gender') }}</th>
-                                    <th scope="col" class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50
+                                        <th scope="col" class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50
                                 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur
                                 backdrop-filter lg:table-cell"> {{ __('Civil status') }}</th>
-                                    <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
+                                        <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
                                 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur
                                 backdrop-filter"> {{ __('Birth date') }}</th>
-                                    <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
+                                        <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
                                 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur
                                 backdrop-filter">
-                                        {{ __('Destination') }}
-                                    </th>
-                                    <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
+                                            {{ __('Destination') }}
+                                        </th>
+                                        <th scope="col"
+                                            class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50
                                 bg-opacity-75 text-right py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
 
-                                    </th>
-                                </tr>
+                                        </th>
+                                    </tr>
                                 </thead>
-                                        <tbody class="bg-white">
-                                        @foreach($records as $record)
-                                            <tr>
-                                                <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm
+                                <tbody class="bg-white">
+                                    @foreach($records as $record)
+                                    <tr>
+                                        <td class="whitespace-nowrap border-b border-gray-200 py-4 pl-4 pr-3 text-sm
                                         font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                                    {{ $record->first_name }} {{ $record->last_name1 }} {{ $record->last_name2 }}</td>
-                                                <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
+                                            {{ $record->first_name }} {{ $record->last_name1 }}
+                                            {{ $record->last_name2 }}</td>
+                                        <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
                                         text-gray-500 hidden sm:table-cell">{{ $record->gender}}</td>
-                                                <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
+                                        <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
                                         text-gray-500 hidden lg:table-cell">{{ $record->civil_status }}</td>
-                                                <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
+                                        <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
                                         text-gray-500">{{ $record->birth_date }}</td>
-                                                <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
+                                        <td class="whitespace-nowrap border-b border-gray-200 px-3 py-4 text-sm
                                         text-gray-500">
-                                                    {{  $record->immigrated_to_place }} {{  $record->immigrated_to_state }}
-                                                </td>
-                                                <td class="relative whitespace-nowrap border-b border-gray-200 py-4 pr-4 pl-3
+                                            {{  $record->immigrated_to_place }}
+                                            {{  $record->immigrated_to_state }}
+                                        </td>
+                                        <td
+                                            class="relative whitespace-nowrap border-b border-gray-200 py-4 pr-4 pl-3
                                                                          text-sm text-right font-medium sm:pr-6 lg:pr-8">
-                                                    @if(auth()->user()->hasRole(['regular user', 'subscriber']))
-                                                        <a href="{{ route('records.show', ['arch'=> $record->archive->id,'id'=>$record->id]) }}" class="inline-flex text-indigo-700
+                                            @if(auth()->user()->hasRole(['regular user', 'subscriber']))
+                                            <a href="{{ route('records.show', ['arch'=> $record->archive->id,'id'=>$record->id]) }}"
+                                                class="inline-flex text-indigo-700
                                                items-center px-3 py-1.5 text-indigo-700">{{ __('Read more') }}</a>
-                                                    @else
-                                                        <a href="{{ route('organizations.archives.show', [$organization, $record->archive,$record]) }}" class="inline-flex text-indigo-700
+                                            @else
+                                            <a href="{{ route('organizations.archives.show', [$organization, $record->archive,$record]) }}"
+                                                class="inline-flex text-indigo-700
                                                items-center px-3 py-1.5 text-indigo-700">{{ __('Read more') }}</a>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
                             </table>
-                            <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-                            {{ $records->links() }}
+                            <div
+                                class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+                                {{ $records->links() }}
                             </div>
                         </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
         </section>
-
     </div>
 </x-app-layout>
