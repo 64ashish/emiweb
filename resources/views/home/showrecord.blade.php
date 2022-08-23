ece8adc53dfd7cca47fb4f5b214c9b047460339b
Ashish <ashish@kortaben.se>
2022-08-23 12:14:26 +0200

    db updated and added archive owners

--- resources/views/home/showrecord.blade.php
+++ resources/views/home/showrecord.blade.php
@@ -75,7 +75,7 @@
                             <h3 class="text-lg leading-6 font-medium text-gray-900 pb-4">Details</h3>
 
                             <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
-                                <dl class="sm:divide-y sm:divide-gray-200 grid grid-cols-1 sm:grid-cols-2">
+                                <dl class="sm:divide-y sm:divide-gray-200 grid grid-cols-1 sm:grid-cols-2 striped">
                                     @foreach($fields as $field)
                                         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                             <dt class="text-sm font-medium text-gray-500">{{ __(ucfirst(str_replace('_', ' ', $field))) }}</dt>
@@ -88,11 +88,11 @@
                                 @if(!empty($detail->user->organization))
                                     <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
                                         <h3 class="text-sm text-gray-500">Archive ownership</h3>
-                                        <div>
-                                            <div>Archive Owner:</div><div>{{ $detail->user->organization->name }}</div>
-                                            <div>Email:</div><div>{{ $detail->user->organization->email }}</div>
-                                            <div>Telephone</div><div>{{ $detail->user->organization->phone }}</div>
-                                        </div>
+
+                                            <div>Owner Name: {{ $detail->user->organization->name }}</div>
+                                            <div>Email: {{ $detail->user->organization->email }}</div>
+                                            <div>Telephone: {{ $detail->user->organization->phone }}</div>
+
                                     </div>
                                 @endif
 
