e9a89997749a64d37fee1d1d2da20372897b538e
Ashish <ashish@kortaben.se>
2022-10-28 12:04:33 +0200

    debuging done pie chart added

--- resources/views/dashboard/swedishchurchemigrationrecord/statistics.blade.php
+++ resources/views/dashboard/swedishchurchemigrationrecord/statistics.blade.php
@@ -172,16 +172,18 @@
                     // barThickness:'10',
                     data: {{ Js::from($data->pluck('total')) }},
                     backgroundColor: poolColors({{ Js::from($data->pluck('total')->count()) }}),
-                    // backgroundColor: [
-                    //     'rgba(255, 99, 132, 0.2)',
-                    //     'rgba(54, 162, 235, 0.2)',
-                    //     'rgba(255, 206, 86, 0.2)',
-                    //     'rgba(75, 192, 192, 0.2)',
-                    //     'rgba(153, 102, 255, 0.2)',
-                    //     'rgba(255, 159, 64, 0.2)'
-                    // ],
 
-                    borderWidth: 1,
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+
+                    borderWidth: 0.5,
                     hoverOffset: 4
                 }]
             },
