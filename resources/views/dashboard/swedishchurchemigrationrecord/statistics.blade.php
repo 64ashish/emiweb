3d1ab0e7f20ae0bd95de30129eef73b565d3bce3
Ashish Aryal <ashisharyal-mbp@MBPsomtrMatilda.localdomain>
2022-12-12 12:23:48 +0100

    provinces sort

--- resources/views/dashboard/swedishchurchemigrationrecord/statistics.blade.php
+++ resources/views/dashboard/swedishchurchemigrationrecord/statistics.blade.php
@@ -65,6 +65,7 @@
                         <div class="mt-1 sm:mt-0 sm:col-span-2">
                             {!! Form::select('from_province', $provinces,null,['class' => 'max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500
                              sm:max-w-xs sm:text-sm border-gray-300 rounded-md',
+                             'placeholder' => 'Alla'
                              ]) !!}
                         </div>
                     </div>
