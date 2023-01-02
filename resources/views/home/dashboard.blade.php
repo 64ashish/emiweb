a482abc4cccd0543eebea58aeb55ee6a905517b0
Ashish <ashish@kortaben.se>
2023-01-02 14:33:06 +0100

    last commit before laravel upgrade

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -8,7 +8,6 @@
                 <div >
                     <div class="pb-6">
                         <ul role="list">
-
                             @if(auth()->user()->hasRole('regular user'))
                                 <li>
                                     <p class="text-sm font-medium text-black-900 px-6 py-4">{{ $catArchives->name }}</p>
@@ -32,8 +31,6 @@
                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                                                                 </svg>
                                                             </a>
-
-
                                                         </div>
                                                         <a href="{{ route('records', $archive) }}">
                                                             {{ __('View archive') }} </a>
