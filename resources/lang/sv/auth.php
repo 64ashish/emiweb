500daca25e78884ce570317aa6e778adc2df99e4
marcus_kinship <mackan.mac3@gmail.com>
2023-05-08 08:08:25 +0200

    Create auth.php
    Add lang for swedish

---
+++ resources/lang/sv/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'failed' => 'Dessa referenser matchar inte våra poster.',
+    'password' => 'Lösenordet som angavs är felaktigt.',
+    'throttle' => 'För många inloggningsförsök. Försök igen om :seconds sekunder.',
+    'email' => 'E-postadressen du angav finns inte i vårt system.',
+    'password_confirmation' => 'Lösenordet matchar inte bekräftelsen.',
+    'reset' => 'Ditt lösenord har återställts!',
+    'sent' => 'Vi har skickat ett e-postmeddelande med länk för att återställa ditt lösenord!',
+    'token' => 'Denna återställningstoken är ogiltig.',
+    'user' => 'Vi kan inte hitta en användare med den angivna e-postadressen.',
+];
