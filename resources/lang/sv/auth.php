91b381f984be4d98c7b5ca980ea19eb10d732062
marcus_kinship <mackan.mac3@gmail.com>
2023-05-08 08:27:23 +0200

    add more
    the entire login page is translated into Swedish

--- resources/lang/sv/auth.php
+++ resources/lang/sv/auth.php
@@ -1,6 +1,31 @@
 <?php
 
 return [
+    // Översättning av "Logga in" på login-sidan
+    'Sign in' => 'Logga in',
+
+    // Översättning av "E-postadress" på login-sidan
+    'Email address' => 'E-postadress',
+
+    // Översättning av "Lösenord" på login-sidan
+    'Password' => 'Lösenord',
+
+    // Översättning av "Kom ihåg mig" på login-sidan
+    'Remember me' => 'Kom ihåg mig',
+
+    // Översättning av "Glömt ditt lösenord?" på login-sidan
+    'Forgot your password?' => 'Glömt ditt lösenord?',
+
+    // Översättning av "Skapa konto" på login-sidan
+    'Create an account' => 'Skapa konto',
+
+    // Översättning av "Registrera" på registration-sidan
+    'Register' => 'Registrera',
+
+     // Översättning av "Registrera" på registration-sidan
+     'or' => 'Eller',
+
+     // Översättning av "Fel" på sidan
     'failed' => 'Dessa referenser matchar inte våra poster.',
     'password' => 'Lösenordet som angavs är felaktigt.',
     'throttle' => 'För många inloggningsförsök. Försök igen om :seconds sekunder.',
