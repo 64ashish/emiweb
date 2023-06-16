da835342dba6b8b77874f58cf6dc14c6d4f09491
marcus_kinship <mackan.mac3@gmail.com>
2023-06-16 18:18:10 +0200

    Update records.blade.php
    Fix name

--- resources/views/dashboard/SwedishAmericanChurchArchiveRecord/records.blade.php
+++ resources/views/dashboard/SwedishAmericanChurchArchiveRecord/records.blade.php
@@ -42,7 +42,7 @@
             function data() {
                 return {
                     openDetails: false,
-                    displayFields:@json($fieldsToDisply),
+                    displayFields:@json($fieldsToDisplay),
                     detail:'',
                     next:'',
                     previous:'',
