bae2b50929e16eda001c4e00e092a28928b64942
marcus_kinship <mackan.mac3@gmail.com>
2023-06-16 18:25:26 +0200

    added comments and order Fields, fix fieldsToDisplay
    * added comments
    *  order Fields
    * fix name on function fieldsToDisply, missing a
    * format

--- app/Models/SwedishAmericanChurchArchiveRecord.php
+++ app/Models/SwedishAmericanChurchArchiveRecord.php
@@ -10,8 +10,13 @@ use Laravel\Scout\Searchable;
 
 class SwedishAmericanChurchArchiveRecord extends Model
 {
-    use HasFactory,  RecordCount;
+    use HasFactory, RecordCount;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'user_id',
         'archive_id',
@@ -48,34 +53,36 @@ class SwedishAmericanChurchArchiveRecord extends Model
         'page',
     ];
 
-
-
-    public function fieldsToDisply()
+    /**
+     * Get the fields to display.
+     *
+     * @return array
+     */
+    public function fieldsToDisplay()
     {
         return [
-            'first_name'=>__(ucfirst(str_replace('_', ' ', 'first_name'))),
-            'last_name'=>__(ucfirst(str_replace('_', ' ', 'last_name'))),
+            'first_name' => __(ucfirst(str_replace('_', ' ', 'first_name'))),
+            'last_name' => __(ucfirst(str_replace('_', ' ', 'last_name'))),
             // 'last_name2'=>__(ucfirst(str_replace('_', ' ', 'last_name2'))),
-
-            'birth_date'=>__(ucfirst(str_replace('_', ' ', 'birth_date'))),
+            'birth_date' => __(ucfirst(str_replace('_', ' ', 'birth_date'))),
             // 'birth_county'=>__(ucfirst(str_replace('_', ' ', 'birth_county'))),
-            'birth_parish'=>__(ucfirst(str_replace('_', ' ', 'birth_parish'))),
-            'birth_province'=>__(ucfirst(str_replace('_', ' ', 'birth_province'))),
-            'immigration_date'=>__(ucfirst(str_replace('_', ' ', 'immigration_date'))),
-            'gender'=>__(ucfirst(str_replace('_', ' ', 'gender'))),
-            'civil_status'=>__(ucfirst(str_replace('_', ' ', 'civil_status'))),
+            'birth_parish' => __(ucfirst(str_replace('_', ' ', 'birth_parish'))),
+            'birth_province' => __(ucfirst(str_replace('_', ' ', 'birth_province'))),
+            'immigration_date' => __(ucfirst(str_replace('_', ' ', 'immigration_date'))),
+            'gender' => __(ucfirst(str_replace('_', ' ', 'gender'))),
+            'civil_status' => __(ucfirst(str_replace('_', ' ', 'civil_status'))),
             // 'emigration_parish'=>__(ucfirst(str_replace('_', ' ', 'emigration_parish'))),
             // 'emigration_province'=>__(ucfirst(str_replace('_', ' ', 'emigration_province'))),
-            'arrival_date_this_place'=>__(ucfirst(str_replace('_', ' ', 'arrival_date_this_place'))),
-            'arrived_from_place'=>__(ucfirst(str_replace('_', ' ', 'arrived_from_place'))),
-            'arrived_from_county'=>__(ucfirst(str_replace('_', ' ', 'arrived_from_county'))),
-            'arrived_from_state'=>__(ucfirst(str_replace('_', ' ', 'arrived_from_state'))),
-            'death_date'=>__(ucfirst(str_replace('_', ' ', 'death_date'))),
+            'arrival_date_this_place' => __(ucfirst(str_replace('_', ' ', 'arrival_date_this_place'))),
+            'arrived_from_place' => __(ucfirst(str_replace('_', ' ', 'arrived_from_place'))),
+            'arrived_from_county' => __(ucfirst(str_replace('_', ' ', 'arrived_from_county'))),
+            'arrived_from_state' => __(ucfirst(str_replace('_', ' ', 'arrived_from_state'))),
+            'death_date' => __(ucfirst(str_replace('_', ' ', 'death_date'))),
             // 'family_nr'=>__(ucfirst(str_replace('_', ' ', 'family_nr'))),
-            'source'=>__(ucfirst(str_replace('_', ' ', 'source'))),
-            'immigrated_to_place'=>__(ucfirst(str_replace('_', ' ', 'immigrated_to_place'))),
-            'immigrated_to_state'=>__(ucfirst(str_replace('_', ' ', 'immigrated_to_state'))),
-            'old_id'=>__(ucfirst(str_replace('_', ' ', 'old_id'))),
+            'source' => __(ucfirst(str_replace('_', ' ', 'source'))),
+            'immigrated_to_place' => __(ucfirst(str_replace('_', ' ', 'immigrated_to_place'))),
+            'immigrated_to_state' => __(ucfirst(str_replace('_', ' ', 'immigrated_to_state'))),
+            'old_id' => __(ucfirst(str_replace('_', ' ', 'old_id'))),
             // 'birth_year'=>__(ucfirst(str_replace('_', ' ', 'birth_year'))),
             // 'birth_month'=>__(ucfirst(str_replace('_', ' ', 'birth_month'))),
             // 'birth_day'=>__(ucfirst(str_replace('_', ' ', 'birth_day'))),
@@ -85,15 +92,17 @@ class SwedishAmericanChurchArchiveRecord extends Model
             // 'death_year'=>__(ucfirst(str_replace('_', ' ', 'death_year'))),
             // 'death_month'=>__(ucfirst(str_replace('_', ' ', 'death_month'))),
             // 'death_day'=>__(ucfirst(str_replace('_', ' ', 'death_day'))),
-            'page'=>__(ucfirst(str_replace('_', ' ', 'page'))),
+            'page' => __(ucfirst(str_replace('_', ' ', 'page'))),
 
-//            'profession'=>__(ucfirst(str_replace('_', ' ', 'profession'))),
-            'id'=>'id',
-            'archive_id'=>__(ucfirst(str_replace('_', ' ', "archive_id")))
+            // 'profession'=>__(ucfirst(str_replace('_', ' ', 'profession'))),
+            'id' => 'id',
+            'archive_id' => __(ucfirst(str_replace('_', ' ', "archive_id"))),
         ];
     }
 
     /**
+     * Get the archive associated with the record.
+     *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function archive()
@@ -101,36 +110,46 @@ class SwedishAmericanChurchArchiveRecord extends Model
         return $this->belongsTo(Archive::class);
     }
 
+    /**
+     * Get the searchable array for the model.
+     *
+     * @return array
+     */
     public function toSearchableArray()
     {
         return [
-            'first_name'=> $this->first_name,
-            'last_name'=> $this->last_name,
-            'last_name2'=> $this->last_name2,
-            'gender'=> $this->gender,
-            'civil_status'=> $this->civil_status,
-            'birth_parish'=> $this->birth_parish,
-            'birth_province'=> $this->birth_province,
-            'emigration_parish'=> $this->emigration_parish,
-            'emigration_province'=> $this->emigration_province,
-            'arrived_from_place'=> $this->arrived_from_place,
-            'arrived_from_county'=> $this->arrived_from_county,
-            'arrived_from_state'=> $this->arrived_from_state,
-            'immigrated_to_place'=> $this->immigrated_to_place,
-            'immigrated_to_state'=> $this->immigrated_to_state,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            // 'last_name2'=> $this->last_name2,
+            'gender' => $this->gender,
+            'civil_status' => $this->civil_status,
+            'birth_parish' => $this->birth_parish,
+            'birth_province' => $this->birth_province,
+            'emigration_parish' => $this->emigration_parish,
+            'emigration_province' => $this->emigration_province,
+            'arrived_from_place' => $this->arrived_from_place,
+            'arrived_from_county' => $this->arrived_from_county,
+            'arrived_from_state' => $this->arrived_from_state,
+            'immigrated_to_place' => $this->immigrated_to_place,
+            'immigrated_to_state' => $this->immigrated_to_state,
             'birth_date' => $this->birth_date,
             'birth_year' => $this->birth_year,
             'death_year' => $this->death_year,
             'immigration_date' => $this->immigration_date,
-            'immigration_year' => $this->immigration_year
+            'immigration_year' => $this->immigration_year,
         ];
     }
 
+    /**
+     * Get the default search fields.
+     *
+     * @return array
+     */
     public function defaultSearchFields()
     {
         return [
-            'first_name',
-            'last_name',
+             'first_name',
+             'last_name',
             'birth_date',
             '---',
             'birth_parish',
@@ -141,25 +160,28 @@ class SwedishAmericanChurchArchiveRecord extends Model
             'family_nr',
             'arrival_date_this_place',
             '---',
-            'immigration_date', 
+            'immigration_date',
             'emigration_parish',
             'emigration_province',
             'immigrated_to_place',
             'immigrated_to_state',
             // ['emigration_province',
             // 'emigration_parish'],
-            'source', 
-            'page', 
+            'source',
+            'page',
             'death_date',
             'id',
         ];
     }
 
-
-
-    public function advancedSearchFields(){
+    /**
+     * Get the advanced search fields.
+     *
+     * @return array
+     */
+    public function advancedSearchFields()
+    {
         return [
-            'last_name2',
             'civil_status',
             'family_nr',
             'arrived_from_place',
@@ -171,35 +193,66 @@ class SwedishAmericanChurchArchiveRecord extends Model
         ];
     }
 
-    public function defaultTableColumns(){
+    /**
+     * Get the default table columns.
+     *
+     * @return array
+     */
+    public function defaultTableColumns()
+    {
         return [
-//            'first_name',
-//            'last_name',
-            'last_name2',
-            'gender',
-            'civil_status',
+            // 'first_name',
+            // 'last_name',
+            'birth_date',
+            'birth_parish',
+            'birth_province',
+            'arrival_date_this_place',
+            'emigration_parish',
             'emigration_province',
-            'arrived_from_county',
-            'immigrated_to_state'
         ];
     }
 
+    /**
+     * Get the formatted birth date attribute.
+     *
+     * @param  string  $value
+     * @return string
+     */
     public function getBirthDateAttribute($value)
     {
         return Carbon::parse($value)->format('Y-m-d');
     }
 
-    public function user(){
+    /**
+     * Get the user associated with the record.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function enableQueryMatch(){
+    /**
+     * Get the fields that should be matched during the query.
+     *
+     * @return array
+     */
+    public function enableQueryMatch()
+    {
         return [
             'first_name',
             'last_name',
         ];
+
+        return [];
     }
 
+    /**
+     * Get the additional search fields.
+     *
+     * @return array
+     */
     public function searchFields()
     {
         return [
