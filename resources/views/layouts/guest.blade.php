6d48baa906d14e9ca80093ace592b2e4dfe9af7e
Marcus Brunström <marcus@kortaben.se>
2023-08-22 08:52:49 -0400

    Reset password

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,7 @@
 <div class="min-h-full md:flex bg-white">
     <div class="md:flex-none md:w-1/2 min-h-screen flex items-center justify-center p-6 md:p-0">
 
+
         <div class="lg:w-2/3 xl:w-1/2">
             {!! Form::open([ 'route'=>['local', http_build_query(request()->except(['_token']))] ]) !!}
             <ul class="text-sm mt-4 font-bold flex justify-end">
@@ -18,16 +19,24 @@
                 <li class="px-1"><button name="language" value="en" type="submit">{{ __('English') }}</button> </li>
             </ul>
             {!! Form::close() !!}
-            <div>
-                <h2 class="text-2xl lg:text-4xl text-gray-900 font-bold mb-3">{{ __('Welcome back') }}</h2>
-                <h4 class="text-gray-600">
-                    @if(Route::is('register') )
-                         {{ __('Register for an account') }}
-                    @else
-                        {{ __('Sign in to your account') }}
+            @if(Route::currentRouteName() == "login")
+                <div>
+
+                    <h2 class="text-2xl lg:text-4xl text-gray-900 font-bold mb-3">{{ __('Welcome back') }}</h2>
+                    <h4 class="text-gray-600">
+                        @if(Route::is('register') )
+                             {{ __('Register for an account') }}
+                        @else
+                            {{ __('Sign in to your account') }}
+                        @endif
+                    </h4>
+                    @if (session('status'))
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ session('status') }}
+                        </div>
                     @endif
-                </h4>
-            </div>
+                </div>
+            @endif
 
             {{ $slot }}
         </div>
