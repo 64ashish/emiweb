c2444410ca9c6ed4f1083b94e2e45e884fadb861
iquallinfo <work@i-quall.com>
2023-12-16 16:57:01 +0530

    organizational subscriber search not working

--- app/Http/Middleware/IsManualSubscriber.php
+++ app/Http/Middleware/IsManualSubscriber.php
@@ -24,16 +24,14 @@ class IsManualSubscriber
         $hasValidDate = !is_null($user->manual_expire) && Carbon::parse($user->manual_expire)->greaterThanOrEqualTo(Carbon::now());
         $manualSubscribed = $user->hasRole('subscriber') && $hasValidDate ;
 
-        if($user->hasRole(['super admin','emiweb admin','emiweb staff','organization admin', 'subscriber', 'organization staff']))
+        if($user->hasRole(['super admin','emiweb admin','emiweb staff','organization admin', 'subscriber', 'organization staff', 'organizational subscriber']))
         {
             return $next($request);
         }
 
         if($stripeSubscribed or $manualSubscribed)
         {
-//            dd("stripe: ".$stripeSubscribed ." manual: ".$manualSubscribed);
             return $next($request);
-
         }
         return redirect('/');
     }
